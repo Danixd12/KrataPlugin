40c2333b580cf345c84cb65eb93a9a9b2190e2d7
Danixd12 <droidgamersyt@gmail.com>
2022-09-04 01:00:57 +0200

    Initial commit

---
+++ src/main/java/net/krataland/krataplugin/KrataPlugin.java
@@ -0,0 +1,17 @@
+package net.krataland.krataplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KrataPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
